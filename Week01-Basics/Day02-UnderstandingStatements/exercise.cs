befb61f7720afaea1f1ebdc14ac74759c4d2a64e
Ante Cvitković <cvitkovicante3@gmail.com>
2026-03-31 17:07:19 +0200

    Add Day 2: Understanding Statements - exercise code
    Day 2 of 90-day C# game dev journey. Learning about sequential statements.

---
+++ Week01-Basics/Day02-UnderstandingStatements/exercise.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Loading game...");
+        Console.WriteLine("Loading complete!");
+        Console.WriteLine("Starting engine...");
+    }
+}
