ea75f458b2b28a9e2c4bb610aeabe160a8922994
Ante Cvitković <cvitkovicante3@gmail.com>
2026-04-01 12:32:54 +0200

    Add Day 3: Variables - exercise code

---
+++ Week01-Basics/Day03-Variables/exercise.cs
@@ -0,0 +1,19 @@
+using System;
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        string playerName;
+        playerName = "Alex";
+        Console.WriteLine(playerName);
+    }
+
+
+
+
+
+}
