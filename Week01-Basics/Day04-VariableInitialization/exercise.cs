8e8df1a17bc5b84b4c431ce5c215805a5f2ad1b8
Ante Cvitković <cvitkovicante3@gmail.com>
2026-04-02 10:21:30 +0200

    Add Day 4: Variable Initialization - exercise code

---
+++ Week01-Basics/Day04-VariableInitialization/exercise.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string guild = "Dragon Slayers";
+        int level = 1;
+
+        Console.WriteLine("Warrior name: " + guild);
+        Console.WriteLine("Warrior level: " + level);
+    }
+}
