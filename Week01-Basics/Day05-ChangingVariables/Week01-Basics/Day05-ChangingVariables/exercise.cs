43e6ef4226b408ba01f38bae1a5cf9075a4d2d37
Ante Cvitković <cvitkovicante3@gmail.com>
2026-04-03 11:14:59 +0200

    Add Day 5: Changing Variables - exercise code

---
+++ Week01-Basics/Day05-ChangingVariables/Week01-Basics/Day05-ChangingVariables/exercise.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int score = 0;
+        Console.WriteLine("Start: " + score);
+        
+        score = 100;
+        Console.WriteLine("After bonus: " + score);
+        
+        score = 250;
+        Console.WriteLine("Final: " + score);
+    }
+}
