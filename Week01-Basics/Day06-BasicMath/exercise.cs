8e29357c8180a797e8b290d8ed97aa44f607c6c4
Ante Cvitković <cvitkovicante3@gmail.com>
2026-04-04 13:46:08 +0200

    Add Day 6: Basic Math - exercise code

---
+++ Week01-Basics/Day06-BasicMath/exercise.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int attack = 10;
+        int bonus = 5;
+        int totalDamage = attack + bonus;
+        
+        Console.WriteLine("Damage: " + totalDamage);
+    }
+}
