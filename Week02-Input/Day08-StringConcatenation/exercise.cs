742950571b1c40e5bc177e254af8eee6e27bf9c0
Ante Cvitković <cvitkovicante3@gmail.com>
2026-04-06 16:32:21 +0200

    Add Day 8: String Concatenation - exercise code

---
+++ Week02-Input/Day08-StringConcatenation/exercise.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string title = "Epic";
+        string type = "RPG";
+        string fullTitle = title + " " + type;
+        
+        Console.WriteLine(fullTitle);
+    }
+}
