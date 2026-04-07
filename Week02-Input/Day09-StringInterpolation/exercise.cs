95dd9d015f4d4ec6614ccf63ecbcc8d16ae8d35f
Ante Cvitković <cvitkovicante3@gmail.com>
2026-04-07 11:34:24 +0200

    Add Day 9: String Interpolation - exercise code

---
+++ Week02-Input/Day09-StringInterpolation/exercise.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string enemy = "Goblin";
+        int hp = 50;
+        
+        Console.WriteLine($"The {enemy} has {hp} HP!");
+    }
+}
