4166d66aa0212c60e1a2d3063b95f6bd9474694a
Ante Cvitković <cvitkovicante3@gmail.com>
2026-04-08 22:06:05 +0200

    Add Day 10: Reading User Input - exercise code

---
+++ Week02-Input/Week02-Input/Day10-ReadingInput/exercise.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter player name: ");
+        string input = Console.ReadLine();
+        
+        Console.WriteLine($"Welcome, {input}!");
+    }
+}
