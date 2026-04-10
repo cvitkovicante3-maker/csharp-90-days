2067f1b9d3714ccc99a733c6c3557cfbb6a4f7c2
Ante Cvitković <cvitkovicante3@gmail.com>
2026-04-10 16:33:47 +0200

    Add Day 12: Constants - exercise code

---
+++ Week02-Input/Day12-Constants/exercise.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        const int MAX_HEALTH = 100;
+        const string GAME_VERSION = "1.0";
+        
+        Console.WriteLine($"Max HP: {MAX_HEALTH}");
+        Console.WriteLine($"Version: {GAME_VERSION}");
+    }
+}
