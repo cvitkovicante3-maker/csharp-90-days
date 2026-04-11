c3d10b5e924e60cde6239e6237b7c35f9092b36d
Ante Cvitković <cvitkovicante3@gmail.com>
2026-04-11 16:34:47 +0200

    Add Day 13: Multiple Variables - exercise code

---
+++ Week02-Input/Day13-MultipleVariables/exercise.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 10, y = 20, z = 30;
+        string a = "Red", b = "Blue";
+        
+        Console.WriteLine($"Position: ({x}, {y}, {z})");
+        Console.WriteLine($"Colors: {a} and {b}");
+    }
+}
