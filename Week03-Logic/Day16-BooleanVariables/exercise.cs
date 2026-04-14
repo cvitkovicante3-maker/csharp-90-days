776c0a25bb0a1f0555cf4293078d97d4df3ca6af
Ante Cvitković <cvitkovicante3@gmail.com>
2026-04-14 09:41:57 +0200

    Add Day 16: Boolean Variables - exercise code

---
+++ Week03-Logic/Day16-BooleanVariables/exercise.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool isAlive = true;
+        bool hasWeapon = false;
+        
+        Console.WriteLine($"Living: {isAlive}");
+        Console.WriteLine($"Armed: {hasWeapon}");
+    }
+}
