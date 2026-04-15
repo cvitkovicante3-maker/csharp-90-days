79999111ce67f29bb5d91bc11b05f1e8d0f7c6a2
Ante Cvitković <cvitkovicante3@gmail.com>
2026-04-15 20:45:55 +0200

    Add Day 17: If Statements - exercise code

---
+++ Week03-Logic/Day17-IfStatements/exercise.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int health = 25;
+        
+        if (health < 50)
+        {
+            Console.WriteLine("Warning: Low health!");
+        }
+    }
+}
