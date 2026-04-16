387123f1039be2ba98e62173ee6f4e13880188ce
Ante Cvitković <cvitkovicante3@gmail.com>
2026-04-16 22:26:11 +0200

    Add Day 18: If-Else - exercise code

---
+++ Week03-Logic/Day18-IfElse/exercise.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int ammo = 0;
+        
+        if (ammo > 0)
+        {
+            Console.WriteLine("Bang!");
+        }
+        else
+        {
+            Console.WriteLine("Click... out of ammo!");
+        }
+    }
+}
