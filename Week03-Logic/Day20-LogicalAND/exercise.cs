8265263fb7e926360f028412730facce57d4436c
Ante Cvitković <cvitkovicante3@gmail.com>
2026-04-18 22:14:16 +0200

    Add Day 20: Logical AND - exercise code

---
+++ Week03-Logic/Day20-LogicalAND/exercise.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool hasSword = true;
+        bool hasShield = true;
+        
+        if (hasSword && hasShield)
+        {
+            Console.WriteLine("Fully equipped!");
+        }
+    }
+}
