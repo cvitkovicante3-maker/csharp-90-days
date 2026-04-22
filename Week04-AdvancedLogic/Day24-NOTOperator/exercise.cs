034d0383384d573f0c7e8508841223423f7ab825
Ante Cvitković <cvitkovicante3@gmail.com>
2026-04-22 21:04:52 +0200

    Add Day 24: NOT Operator - exercise code

---
+++ Week04-AdvancedLogic/Day24-NOTOperator/exercise.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool isPaused = false;
+        
+        if (!isPaused)
+        {
+            Console.WriteLine("Game is running");
+        }
+    }
+}
